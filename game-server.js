e4822a976ba32766285c338b281b996ea525328d
Ivan Berlin <ivan.berlin06@gmail.com>
2023-09-29 14:53:20 +0300

    removed useless console.log

--- game-server.js
+++ game-server.js
@@ -13,7 +13,6 @@ const server = net.createServer((socket) => {
       let params = message.range.split("-");
       MIN_RANGE = parseInt(params[0]);
       MAX_RANGE = parseInt(params[1]);
-      console.log(MIN_RANGE, MAX_RANGE);
       socket.write(JSON.stringify({ answer: Math.floor((MIN_RANGE + MAX_RANGE) / 2) }));
     }
 
